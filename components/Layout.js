3c3126bf4d8f89f4f79bbc88f45278dbe74297dd
sumito <sumito.temma@gmail.com>
2023-01-12 01:31:51 +0900

    ISR処理追加

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,13 @@
 import Head from "next/head";
 
-export default function Layout({ children, title= "Defai;t tot;e"}){
+export default function Layout({ children, title= "Default title"}){
     return(
         <div className="flex justify-center items-center flex-col min-h-screen text-white font-mono bg-gray-800">
             <Head>
                 <title>{title}</title>
             </Head>
-            <main className="flex flex-1 justify-center items-center w-screen flex-col">
+            <main className="flex ">
+                
                 {children}
             </main>
 
--- pages/main-page.js
+++ pages/main-page.js
@@ -16,20 +16,21 @@ export default function MainPage() {
 
     return (
         <Layout title="Main page">
-            <div className="mb-10">
+            <div className="mb-10 flex">
+                
                 <Link href="/blog-page">
                     <p className="bg-indigo-500 mr-8 hover:bg-indigo-600 text-white px-4 py-12 rounded">Visit BLOG BY SSG + ISR</p>
                 </Link>
+                
                 <Link href="/task-page">
                     <p className="bg-gray-500 mr-8 hover:bg-indigo-600 text-white px-4 py-12 rounded">Visit Task BY SSG + ISR</p>
                 </Link>
             </div>
 
 
-
             <svg 
             onClick={logout}
-            className="w-6 h-6" 
+            className="mt-10 cursor-pointer w-6 h-6" 
             fill="none" 
             stroke="currentColor" 
             viewBox="0 0 24 24" 
